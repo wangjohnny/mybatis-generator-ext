d0c766968c7183c9ea4b5074ec55d84c8325608e
lao king <wangjian5748@gmail.com>
2025-09-11 15:19:55 +0800

    给 boolean 类型添加默认值

--- mybatis-generator-plugin/src/main/java/com/github/mybatis/generator/plugin/BooleanColumnPlugin.java
+++ mybatis-generator-plugin/src/main/java/com/github/mybatis/generator/plugin/BooleanColumnPlugin.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
 
 /**
  * 
@@ -26,7 +28,26 @@ public class BooleanColumnPlugin extends PluginAdapter {
     public boolean validate(List<String> warnings) {
         return true;
     }
+    
+    @Override
+    public boolean modelFieldGenerated(Field field,
+            TopLevelClass topLevelClass,
+            IntrospectedColumn introspectedColumn,
+            IntrospectedTable introspectedTable, 
+            ModelClassType modelClassType) {
 
+        if (introspectedColumn.getActualColumnName().toLowerCase().startsWith(PREFIX_STRING)
+                && introspectedColumn.getLength() == 1) {
+            // 设置Java字段的默认值
+            field.setInitializationString("false");
+            // 添加默认值注解
+            field.addAnnotation("@Builder.Default");
+            topLevelClass.addImportedType("lombok.Builder");
+        }
+
+        return true;
+    }
+    
     @Override
     public void initialized(IntrospectedTable introspectedTable) {
         List<IntrospectedColumn> introspectedColumns = introspectedTable.getAllColumns();
